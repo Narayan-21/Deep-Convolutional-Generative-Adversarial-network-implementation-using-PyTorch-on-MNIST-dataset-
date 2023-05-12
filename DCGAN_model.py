139166b2534b1a5c0ceb22040b2ebdd051199117
Narayan-21 <naaidjan.19@gmail.com>
2023-05-12 07:04:26 +0530

    DCGAN_1

---
+++ DCGAN_model.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, channels_noise, channels_img, features_g):
+        super(Generator, self).__init__()
+        self.net = nn.Sequential(
+            self.gen_block(channels_noise, features_g * 16, 4, 1, 0),
+            self.gen_block(features_g * 16, features_g * 8, 4, 2, 1),
+            self.gen_block(features_g * 8, features_g * 4, 4, 2, 1),
+            self.gen_block(features_g * 4, features_g * \
+                           2, 4, 2, 1),
+            nn.ConvTranspose2d(
+                features_g * 2, channels_img, kernel_size=4, stride=2, padding=1
+            ),
+            nn.Tanh(),
+        )
+
+    def gen_block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, channels_img, features_d):
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+            nn.Conv2d(channels_img, features_d,
+                      kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            self.disc_block(features_d, features_d * 2, 4, 2, 1),
+            self.disc_block(features_d * 2, features_d * 4, 4, 2, 1),
+            self.disc_block(features_d * 4, features_d * 8, 4, 2, 1),
+            nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0),
+            nn.Sigmoid(),
+        )
+
+    def disc_block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2),
+        )
+
+    def forward(self, x):
+        return self.disc(x)
+
+# As per the DCGAN paper: All the weights are initialized from a zero centered normal distribution with standard deviation 0.02
+def initialize_weights(model):
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
